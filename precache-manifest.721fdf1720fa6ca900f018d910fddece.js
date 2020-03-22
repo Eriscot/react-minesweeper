3b7bed7105536b200190891c20943f7be9eb8be0
Alexander Shakhovskiy <AShahouski@gomel.iba.by>
2020-03-23 00:34:11 +0300

    Updates

---
+++ precache-manifest.721fdf1720fa6ca900f018d910fddece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9336e1bd87aa27760f7b9d7d9c1d5f5",
+    "url": "/react-minesweeper/index.html"
+  },
+  {
+    "revision": "110f1e3d136d6e05a16c",
+    "url": "/react-minesweeper/static/css/main.005bfcd8.chunk.css"
+  },
+  {
+    "revision": "9eabf88c3691c8e2355d",
+    "url": "/react-minesweeper/static/js/2.78f281ec.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-minesweeper/static/js/2.78f281ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "110f1e3d136d6e05a16c",
+    "url": "/react-minesweeper/static/js/main.18b9cd1d.chunk.js"
+  },
+  {
+    "revision": "2b5af294c86d3660d8fd",
+    "url": "/react-minesweeper/static/js/runtime-main.b154da04.js"
+  }
+]);
